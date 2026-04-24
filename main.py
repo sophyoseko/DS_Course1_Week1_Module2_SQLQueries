a09ae4dc01aeba828c54efa7318e235df19d2ee4
sophyoseko <sophyoseko@gmail.com>
2026-04-24 11:36:15 +0300

    made changes to main.py

--- main.py
+++ main.py
@@ -17,40 +17,37 @@ df_no_moons = pd.read_sql("""
                         SELECT * FROM planets
                         WHERE num_of_moons = 0;  
                           """, conn1)
-df_no_moons
+
 # CodeGrade step2
 # Replace None with your code
 df_name_seven = pd.read_sql("""
-                        SELECT name, mass
-                        FROM planets
+                        SELECT name, mass FROM planets
                         WHERE LENGTH(name)= 7;   
                             """, conn1)
-df_name_seven
+
 # CodeGrade step3
 # Replace None with your code
 df_mass = pd.read_sql("""
-                        SELECT name, mass
-                        FROM planets
+                        SELECT name, mass FROM planets
                         WHERE mass <= 1.00;   
                             """, conn1)
-df_mass
+
 # CodeGrade step4
 # Replace None with your code
 df_mass_moon = pd.read_sql("""
-                    SELECT *
-                    FROM planets
-                    WHERE mass < 1.00 AND num_of_moons >= 1      
-                           """, conn1)
-df_mass_moon
+              SELECT * FROM planets
+              WHERE num_of_moons >= 1
+              AND mass < 1.00;
+              """, conn1)
+
 # CodeGrade step5
 # Replace None with your code
 df_blue = pd.read_sql("""
-                SELECT name, color
-                FROM planets
-                WHERE color = "blue"
+                SELECT name, color FROM planets
+                WHERE color LIKE '%blue';
                       
                       """, conn1)
-df_blue
+
 # CodeGrade step0
 
 # Run this cell without changes
@@ -64,35 +61,31 @@ pd.read_sql("SELECT * FROM dogs;", conn2)
 # CodeGrade step6
 # Replace None with your code
 df_hungry = pd.read_sql("""
-                    SELECT name, age, breed 
-                    FROM dogs
+                    SELECT name, age, breed FROM dogs
                     WHERE hungry= 1 
                     ORDER BY age ASC;   
                         """, conn2)
-df_hungry
+
 # CodeGrade step7
 # Replace None with your code
 df_hungry_ages = pd.read_sql("""
-                        SELECT name, age, hungry
-                        FROM dogs
-                        WHERE hungry= 1 AND age BETWEEN 2 AND 7
+                        SELECT name, age, hungry FROM dogs
+                        WHERE hungry= 1
+                        AND age BETWEEN 2 AND 7
                         ORDER BY name ASC;
                              
                              """, conn2)
-df_hungry_ages
+
 # CodeGrade step8
 # Replace None with your code
 df_4_oldest = pd.read_sql("""
 SELECT name, age, breed
 FROM (
-    SELECT name, age, breed
-    FROM dogs
+    SELECT name, age, breed FROM dogs
     ORDER BY age DESC
-    LIMIT 4
-)
-ORDER BY breed ASC;
+    LIMIT 4;
 """, conn2)
-df_4_oldest
+
 # CodeGrade step0
 
 # Run this cell without changes
@@ -107,35 +100,34 @@ SELECT * FROM babe_ruth_stats; """, conn3)
 # CodeGrade step9
 # Replace None with your code
 df_ruth_years = pd.read_sql("""
-                        SELECT COUNT(DISTINCT year) AS total_years
-                        FROM babe_ruth_stats
+                        SELECT COUNT(year) AS total_years FROM babe_ruth_stats;
                            
                             """, conn3)
-df_ruth_years
+
 # CodeGrade step10
 # Replace None with your code
 df_hr_total = pd.read_sql("""
-                    SELECT  SUM(HR) AS homeruns
-                    FROM babe_ruth_stats    
+                    SELECT  SUM(HR) AS homeruns FROM babe_ruth_stats    
                           """, conn3)
-df_hr_total
+
 # CodeGrade step11
 # Replace None with your code
 df_teams_years = pd.read_sql("""
-                        SELECT team, COUNT (year) AS number_year
+                        SELECT team, COUNT (year) AS number_years 
                         FROM babe_ruth_stats 
                         GROUP BY team;    
                              """, conn3)
-df_teams_years
+
 # CodeGrade step12
 # Replace None with your code
 df_at_bats = pd.read_sql("""
                     SELECT team, AVG(at_bats) AS average_at_bats
                     FROM babe_ruth_stats 
                     GROUP BY team 
-                    HAVING average_at_bats > 200    
+                    HAVING AVG(at_bats)> 200    
                          """, conn3)
-df_at_bats
+
+# Run this cell without changes
 
 conn1.close()
 conn2.close()
